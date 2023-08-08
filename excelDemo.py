10c87e16e9f423a7fcff3cc537cb1088660a81cb
Shinjini Sanyal <paramitasanyalghosal511@gmail.com>
2023-08-08 15:04:36 +0530

    first commit in dev branch via GitDemo user

--- excelDemo.py
+++ excelDemo.py
@@ -1,5 +1,6 @@
 #THIS IS THE ALTERED VERSION OF THE CODE DOWNLOADED/CLONED FROM ORIGINAL CODE IN REMOVE GITHUB REPO
 #2nd time alteration done by user GitA after downloading the altered code uploaded by GitDemo to remote repo
+#USER NO 2 CHECKED OUT THE LATEST CODE FROM MASTER BRANCH INTO A NEW DEV BRANCH, MODIFIED CODE AND MERGED IT TO MASTER
 
 import openpyxl as op
 
@@ -13,5 +14,7 @@ for row in range(1,sheet.max_row+1):
         for column in range(2,sheet.max_column+1):
             #print(sheet.cell(row=row,column=column).value)
             data_dict[sheet.cell(row=1,column=column).value]=sheet.cell(row=row,column=column).value
+            
 print("THIS IS THE ALTERED VERSION OF THE CODE DOWNLOADED/CLONED FROM ORIGINAL CODE IN REMOVE GITHUB REPO")
 print("2ND TIME ALTERATION DONE BY USER GITA AFTER DOWNLOADING THE ALTERED CODE UPLOADED BY GITDEMO TO REMOTE REPO")
+print("USER NO 2 CHECKED OUT THE LATEST CODE FROM MASTER BRANCH INTO A NEW DEV BRANCH, MODIFIED CODE AND MERGED IT TO MASTER")
